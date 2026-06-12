c6e4eeeb3321cad602934e14d916cc4e94efb3a0
Nhi Le <ilenhi2710@gmail.com>
2026-06-12 22:50:33 +0700

    learn: Basic javascript clouser advanced

--- day-1/counter.ts
+++ day-1/counter.ts
@@ -1,7 +1,8 @@
 function createCounter() {
     let count = 0;
     const counter = function() {
-        return count++;
+         count++;
+         return count;
     }
     return counter;
 }
@@ -17,3 +18,45 @@ console.log(counter2());
 console.log(counter2());
 console.log(counter2());
 console.log(counter2());
+
+console.log("==============Advanced Counter==================");
+
+function createCounterAdvanced() {
+    let count = 0;
+    return {
+        increment: () => ++count,
+        decrement: () => --count,
+        reset: () => {
+            count = 0;
+        },
+        getValue: () => count
+    };
+}
+
+const counter3 = createCounterAdvanced();
+console.log(counter3.getValue()); // 0
+console.log(counter3.increment()); // 1
+console.log(counter3.increment()); // 2
+console.log(counter3.decrement()); // 1
+counter3.reset();
+console.log(counter3.getValue()); // 0
+
+console.log("==============Private Counter with IIFE (Immediately Invoked Function Expression)==================");
+
+const counterModule = (function() {
+    let count = 0;
+    
+    function increment() {
+        return ++count;
+    }
+    function getCount() {
+        return count;
+    }
+
+    return { increment, getCount };
+})();
+
+console.log(counterModule.getCount()); // 0
+console.log(counterModule.increment()); // 1
+console.log(counterModule.increment()); // 2
+console.log(counterModule.getCount()); // 2
