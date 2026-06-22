1a4c85881c3607cacc03217cc130204a73e9505b
Nhi Le <ilenhi2710@gmail.com>
2026-06-22 18:45:48 +0700

    Day 7: Improve code isValidBetter

--- day-7/valid-parentheses.ts
+++ day-7/valid-parentheses.ts
@@ -50,7 +50,7 @@ var isValidBetter = function(s:string) {
     for (const char of s) {
         if(char === '(' || char === "[" || char === "{")
             stack.push(char)
-        else if(stack.pop() !== pairs[char])
+        else if(!stack.length && stack.pop() !== pairs[char])
             return false
     }
     return stack.length === 0;
